9d6eb054ff446627ab5e09172c47c8df27ae5918
VAAN <vaan.origin@gmail.com>
2024-09-12 13:32:51 +0530

    Project 3 : Currency Convertor started. minor changes in the remaining previous folders and added some notes in readme files .

---
+++ 5_React_Projects/3.currencyConvertor/README.md
@@ -0,0 +1,6 @@
+# Currreny Convertor
+
+## Important things ,I learned while creating this project.
+
+1. Custom Hooks
+2. 
---
+++ 5_React_Projects/3.currencyConvertor/src/App.jsx
@@ -0,0 +1,16 @@
+// CUSTOM REACT HOOK
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold bg-orange-600">
+      Currency Convertor
+    </h1>
+    </>
+  )
+}
+
+export default App
